f54936cf5bbfd0da656293afae21bd83fc4b4d64
rsm28 <91552832+rsm28@users.noreply.github.com>
2023-12-07 00:19:14 +0000

    Update RUNME.bat
    THIS SUCKS

--- RUNME.bat
+++ RUNME.bat
@@ -1,108 +1,93 @@
 @echo off
 setlocal enabledelayedexpansion
+REM 1. see whether 'Lethal Company.exe' exists in the current directory
+REM 2. if it doesn't, we do the registry things and backup installation finding
+REM 3. after we verify we're in the right directory:
+    REM 3.1. pull latest: modlist.txt, backup version.bat to the current directory
+    REM 3.2. run backup version.bat
 
-REM Read mods from modlist.txt
+REM Check if 'Lethal Company.exe' exists in the current directory.
+IF EXIST "%~dp0\Lethal Company.exe" (
+    SET "LC_PATH=%~dp0"
+) ELSE (
+    ECHO Looking for Steam installation folder...
+    REM Find Steam installation.
+    FOR /F "Tokens=1,2*" %%A IN ('Reg Query HKCU\Software\Valve\Steam') DO (
+        IF "%%A" EQU "SteamPath" SET "STEAMPATH=%%C"
+    )
+
+    IF NOT DEFINED STEAMPATH (
+        FOR /F "Tokens=1,2*" %%A IN ('Reg Query HKLM\SOFTWARE\WOW6432Node\Valve\Steam') DO (
+            IF "%%A" EQU "InstallPath" SET "STEAMPATH=%%C"
+        )
+    )
+
+    IF NOT DEFINED STEAMPATH (
+        FOR /F "Tokens=1,2*" %%A IN ('Reg Query HKLM\SOFTWARE\Valve\Steam') DO (
+            IF "%%A" EQU "InstallPath" SET "STEAMPATH=%%C"
+        )
+    )
+
+   IF "!STEAMPATH:~-1!"=="\" SET "STEAMPATH=!STEAMPATH:~0,-1!"
+   SET "STEAMPATH=!STEAMPATH:/=\!"
+   SET "LC_PATH=!STEAMPATH!\steamapps\common\Lethal Company"
+
+   IF NOT EXIST "!LC_PATH!\Lethal Company.exe" (
+       ECHO Lethal Company installation not found. Exiting...
+       timeout /t 5 >nul
+       pause
+       GOTO :EOF
+   )
+)
+
+ECHO Lethal Company installation found at !LC_PATH!.
+ECHO Removing modlist.txt and backup_version.bat
+if exist "!LC_PATH!\modlist.txt" del "!LC_PATH!\modlist.txt"
+if exist "!LC_PATH!\backup_version.bat" del "!LC_PATH!\backup_version.bat"
+
+REM Pull latest modlist.txt and backup_version.bat
+ECHO Downloading latest modlist.txt and backup_version.bat...
+powershell.exe -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rsm28/lethal_company_batch_files/main/modlist.txt' -OutFile '!LC_PATH!\modlist.txt'}"
+powershell.exe -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rsm28/lethal_company_batch_files/main/BackupVersion.bat' -OutFile '!LC_PATH!\backup_version.bat'}"
+
+REM Read the mods from the mods.txt file.
+ECHO ---
 SET /A MOD_INDEX=0
 FOR /F "tokens=*" %%A IN (modlist.txt) DO (
     SET "MODS[!MOD_INDEX!]=%%A"
     SET /A MOD_INDEX+=1
 )
 
-IF NOT EXIST "%~dp0\Lethal Company.exe" (
-    ECHO Lethal Company.exe not found in current directory.
-    ECHO Run this script from the same folder as Lethal Company.exe.
-    timeout /t 5 >nul
-    pause
-    GOTO :EOF
+REM Go through each mod and...
+FOR /L %%I IN (0,1,!MOD_INDEX!-1) DO (
+    SET "MOD_INFO=!MODS[%%I]!"
+    FOR /F "tokens=1,2 delims=-" %%A IN ("!MOD_INFO!") DO (
+        SET "AUTHOR=%%A"
+        SET "MODNAME=%%B"
+        REM Call the API and parse the JSON respons.
+        FOR /F "delims=" %%V IN ('powershell -Command "$response = Invoke-RestMethod -Uri 'https://thunderstore.io/api/experimental/package/!AUTHOR!/!MODNAME!/'; $response.latest.version_number"') DO (
+            SET "LATEST_VERSION[%%I]=%%V"
+        )
+        ECHO !AUTHOR!-!MODNAME!-!LATEST_VERSION[%%I]! is the latest version.
+    )
 )
 
-ECHO Removing existing mod files...
-REM Step -1: Remove all existing mod files if they exist!
-if exist "doorstop_config.ini" del /q "doorstop_config.ini"
-if exist "winhttp.dll" del /q "winhttp.dll"
-if exist "BepInEx\" rmdir /s /q "BepInEx"
-if exist "Local_Downloads\" rmdir /s /q "Local_Downloads"
-
-ECHO Creating Local_Downloads directory...
-REM Step 0: Create a directory called "Local_Downloads"
-mkdir Local_Downloads
-
-ECHO Downloading BepInExPack...
-REM Step 1: Download BepInExPack and extract it
-powershell.exe -Command "& {Invoke-WebRequest -Uri 'https://thunderstore.io/package/download/BepInEx/BepInExPack/5.4.2100/' -OutFile '.\Local_Downloads\BepInExPack_5.4.2100.zip'}"
-
-ECHO Extracting BepInExPack...
-REM Step 2: Extract the zip file into "Local_Downloads\Extract"
-powershell.exe -Command "& {Expand-Archive -Path '.\Local_Downloads\BepInExPack_5.4.2100.zip' -DestinationPath '.\Local_Downloads\Extract'}"
-
-ECHO Moving BepInEx contents to root folder...
-REM Step 3: Move contents of BepInExPack to root folder
-xcopy ".\Local_Downloads\Extract\BepInExPack" . /E /I /Y /q
-
-ECHO Launching LC to install BepInEx...
-REM Step 4: Launch and close Lethal Company.exe
-start "" /B "Lethal Company.exe"
-timeout /t 5 >nul
-taskkill /im "Lethal Company.exe" /f
-
-ECHO Cleaning up Extract directory for plugin extraction...
-REM Step 5: Delete Local_Downloads\Extract for clean plugin extraction
-rmdir /s /q "Local_Downloads\Extract"
-
-REM Main mod loop
-SET /A MOD_INDEX=0
-:MOD_LOOP
-IF NOT DEFINED MODS[%MOD_INDEX%] GOTO :END_MOD_LOOP
-SET CURRENT_MOD=!MODS[%MOD_INDEX%]!
-CALL :DOWNLOAD_AND_PROCESS_MOD !CURRENT_MOD!
-SET /A MOD_INDEX+=1
-GOTO :MOD_LOOP
-:END_MOD_LOOP
-
-ECHO ---
-
-ECHO Cleaning up unnecessary files from Extract folder after extraction...
-if exist ".\Local_Downloads\Extract\icon.png" del /q ".\Local_Downloads\Extract\icon.png" 2>nul
-if exist ".\Local_Downloads\Extract\manifest.json" del /q ".\Local_Downloads\Extract\manifest.json" 2>nul
-del /q ".\Local_Downloads\Extract\*.md" 2>nul
-del /q ".\Local_Downloads\Extract\*.zip" 2>nul
-
-ECHO Moving contents from .\Local_Downloads\Extract\BepInEx to .\BepInEx...
-REM this for loop just moves errant .dlls from shit mod authors into the correct folder
-FOR /R ".\Local_Downloads\Extract\" %%G IN (*.dll) DO (
-    move "%%G" ".\Local_Downloads\Extract\BepInEx\plugins\" >nul 2>&1
+REM Update the modlist.txt file with the latest version numbers.
+COPY /Y NUL modlist.tmp >nul 2>&1
+FOR /L %%I IN (0,1,!MOD_INDEX!-1) DO (
+    SET "MOD_INFO=!MODS[%%I]!"
+    FOR /F "tokens=1,2 delims=-" %%A IN ("!MOD_INFO!") DO (
+        SET "AUTHOR=%%A"
+        SET "MODNAME=%%B"
+        ECHO !AUTHOR!-!MODNAME!-!LATEST_VERSION[%%I]!>> modlist.tmp
+    )
 )
-REM instead of moving .dlls, we now just merge the two folders - now we can use mods that have additional files (like cosmetic suit mods or whatever)
-xcopy ".\Local_Downloads\Extract\BepInEx\*" ".\BepInEx\" /E /Y /Q >nul 2>&1
+MOVE /Y modlist.tmp modlist.txt >nul 2>&1
 
+ECHO Modlist updated with the latest version numbers.
 ECHO ---
-ECHO ---
-ECHO ---
-ECHO You can run the game now!
-ECHO Take a drink! 
-ECHO ---
-ECHO ---
-ECHO ---
-pause
-GOTO :EOF
+ECHO Running installer...
 
-
-:::::::::::::::::::::::::::::::
-:DOWNLOAD_AND_PROCESS_MOD
-SET MOD_INFO=%1
-FOR /F "tokens=1-3 delims=-" %%A IN ("%MOD_INFO%") DO (
-    SET MOD_AUTHOR=%%A
-    SET MOD_NAME=%%B
-    SET VERSION=%%C
-)
-
-ECHO ---
-ECHO Downloading !MOD_NAME! version !VERSION! from author !MOD_AUTHOR!...
-SET WEBREQUEST_URL=https://thunderstore.io/package/download/!MOD_AUTHOR!/!MOD_NAME!/!VERSION!/
-ECHO !WEBREQUEST_URL!
-powershell.exe -Command "& {Invoke-WebRequest -Uri '!WEBREQUEST_URL!' -OutFile '.\Local_Downloads\!MOD_NAME!_!VERSION!.zip'}"
-
-ECHO Extracting !MOD_NAME! version !VERSION!...
-powershell.exe -Command "& {Expand-Archive -Path '.\Local_Downloads\!MOD_NAME!_!VERSION!.zip' -DestinationPath '.\Local_Downloads\Extract' -Force}"
-GOTO :EOF
-:::::::::::::::::::::::::::::::
+REM Run backup version.bat
+ECHO Running backup_version.bat...
+call "!LC_PATH!\backup_version.bat"
